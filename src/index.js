cc3cd9370d9c622f5e239050f25cbf48374326e6
Stephen Fraser <stephen.fraser@digirati.com>
2018-04-27 21:33:28 +0100

    Netlify cms test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import CMS from 'netlify-cms'
+
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
+
 import './index.css'
 
 import React from 'react'
